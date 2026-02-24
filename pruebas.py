dc724decd369d47259f066b8b14753234e05b9cc
rcburbanoo <60909923+rcburbanoo@users.noreply.github.com>
2026-02-24 00:01:58 -0500

    se creo pruebas.py y el test

---
+++ pruebas.py
@@ -0,0 +1,10 @@
+def suma(a,b):
+    return a + b
+
+
+
+def promedio(lista):
+    suma=0
+    for i in lista:
+        suma=suma + i
+    return suma/len(lista)
---
+++ test_pruebas.py
@@ -0,0 +1,8 @@
+import pruebas
+
+def test_suma():
+    assert pruebas.suma(2, 3) == 5
+
+def test_promedio():
+    numeros = [1, 2, 3, 4, 5]
+    assert pruebas.promedio(numeros) == 3.0 
